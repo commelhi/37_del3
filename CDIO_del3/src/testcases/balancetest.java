8f6527b3cfab591e95cc2a034fc70490c1fd8365
Aslanbek <commelhi@gmail.com>
2015-11-27 16:35:17 +0100

    balancetest mere kød på

--- CDIO_del3/src/testcases/balancetest.java
+++ CDIO_del3/src/testcases/balancetest.java
@@ -4,17 +4,25 @@ import entity.Account;
 
 public class balancetest {
 
-	private static Account acc = new Account();
-	
-	public balancetest(String args[]){
-		
-		
-		acc.addToBalance(7);
-		
-		
-		acc.getBalance();
-			
+	static Account acc = new Account();
+
+	public static void main(String args[]){
+
+		if(test1()){
+			System.out.println("ok:" + test1());
+		};
+
 	}
-	
-	
+
+
+	private static boolean test1(){
+		int b = -8;
+		b = acc.getBalance();
+
+		return acc.addToBalance(4);
+
+
+	}
+
+
 }
